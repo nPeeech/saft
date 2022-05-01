5648942dea3f257957ae3c4ce0739ebdef3e3c58
nPeeech <33167904+nPeeech@users.noreply.github.com>
2022-05-01 11:01:22 +0900

    spinnerがsharedPreferencesから値を設定するようにした

--- app/src/main/java/io/github/npeeech/saft/ChoiceFragment.kt
+++ app/src/main/java/io/github/npeeech/saft/ChoiceFragment.kt
@@ -1,10 +1,13 @@
 package io.github.npeeech.saft
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import io.github.npeeech.saft.databinding.FragmentChoiceBinding
@@ -31,8 +34,25 @@ class ChoiceFragment : Fragment() {
         binding.sharedViewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+        val alarmOffsetDefaultValue = resources.getInteger(R.integer.alarmOffsetDefaultValue)
+        val alarmOffsetValue =
+            sharedPref?.getInt(getString(R.string.alarmOffsetKey), alarmOffsetDefaultValue)
+
         val adapter = AlarmListAdapter(viewModel.alarmList)
         binding.alarmList.adapter = adapter
+
+        val spinner: Spinner = binding.alarmOffsetSpinner
+
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.offset_time_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+            alarmOffsetValue?.let { spinner.setSelection(it) }
+        }
 
         return binding.root
     }
