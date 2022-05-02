334009bc7aaf5a842ce477dd00a0f1f8a9778af8
nPeeech <33167904+nPeeech@users.noreply.github.com>
2022-05-02 15:04:57 +0900

    時刻部分をspanで装飾できた

--- app/src/main/java/io/github/npeeech/saft/SharedViewModel.kt
+++ app/src/main/java/io/github/npeeech/saft/SharedViewModel.kt
@@ -1,14 +1,17 @@
 package io.github.npeeech.saft
 
+import android.graphics.Color
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import java.time.LocalTime
-import java.util.stream.Collectors
 
 class SharedViewModel(receiveText: String) : ViewModel() {
     val plainText = MutableLiveData<String>()
+    val richText = MutableLiveData<SpannableString>()
 
     private val _eventParse = MutableLiveData<Boolean>()
     val eventParse: LiveData<Boolean>
@@ -39,12 +42,23 @@ class SharedViewModel(receiveText: String) : ViewModel() {
         alarmList.value = regex.findAll(plainText.value ?: "").map {
             LocalTime.parse(it.value)
         }.toSet().toList().sorted()
+
+        richText.value = SpannableString(plainText.value)
+        regex.findAll(plainText.value ?: "").forEach {
+            richText.value?.setSpan(
+                ForegroundColorSpan(Color.RED),
+                it.range.start,
+                it.range.endInclusive+1,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
     }
 
     fun calcAlarm(alarmTime: LocalTime): LocalTime {
         return alarmTime.plusMinutes(deltaTimeList[deltaTimePosition.value ?: 0].toLong())
     }
-    fun calcAlarm(position: Int): LocalTime{
+
+    fun calcAlarm(position: Int): LocalTime {
         val alarmTime = alarmList.value?.get(position)!!
         return calcAlarm(alarmTime)
     }
