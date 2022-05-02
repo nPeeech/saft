767f64b32ed1dddd924a0d72c0898228873757ce
nPeeech <33167904+nPeeech@users.noreply.github.com>
2022-05-02 21:20:57 +0900

    きれいにした

--- app/src/main/java/io/github/npeeech/saft/ChoiceFragment.kt
+++ app/src/main/java/io/github/npeeech/saft/ChoiceFragment.kt
@@ -5,8 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.provider.AlarmClock
 import android.text.method.LinkMovementMethod
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,15 +13,11 @@ import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import io.github.npeeech.saft.databinding.FragmentChoiceBinding
-import java.time.LocalTime
 
 class ChoiceFragment : Fragment(), AdapterView.OnItemSelectedListener {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     private lateinit var binding: FragmentChoiceBinding
     private val viewModel: SharedViewModel by activityViewModels()
@@ -46,30 +40,13 @@ class ChoiceFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
         val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
         val alarmOffsetDefaultValue = resources.getInteger(R.integer.alarmOffsetDefaultValue)
-        viewModel.deltaTimeList = resources.getIntArray(R.array.offset_time_integer_array).toList()
-        viewModel.deltaTimePosition.value = sharedPref?.getInt(getString(R.string.alarmOffsetKey), alarmOffsetDefaultValue)
-
-
-//        val adapter = AlarmListAdapter(object : ClickButtonListener{
-//            override fun onClick(position: Int) {
-//                val hour = viewModel.calcAlarm(position).hour.toInt()
-//                val minutes = viewModel.calcAlarm(position).minute.toInt()
-//                val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
-//                    putExtra(AlarmClock.EXTRA_HOUR, hour)
-//                    putExtra(AlarmClock.EXTRA_MINUTES, minutes)
-//                    putExtra(AlarmClock.EXTRA_SKIP_UI, true)
-//                }
-//                if (intent.resolveActivity(requireActivity().packageManager) != null){
-//                    startActivity(intent)
-//                }
-//                Toast.makeText(activity, "set $hour : $minutes", Toast.LENGTH_SHORT).show()
-//            }
-//        })
-//        binding.alarmList.adapter = adapter
-
+        viewModel.setDeltaTimeList(
+            resources.getIntArray(R.array.offset_time_integer_array).toList()
+        )
+        viewModel.deltaTimePosition.value =
+            sharedPref?.getInt(getString(R.string.alarmOffsetKey), alarmOffsetDefaultValue)
 
         val spinner: Spinner = binding.alarmOffsetSpinner
-
         ArrayAdapter.createFromResource(
             requireContext(),
             R.array.offset_time_string_array,
@@ -81,34 +58,36 @@ class ChoiceFragment : Fragment(), AdapterView.OnItemSelectedListener {
             spinner.onItemSelectedListener = this
         }
 
-        viewModel.deltaTimePosition.observe(viewLifecycleOwner, androidx.lifecycle.Observer { position ->
-            viewModel.alarmList.value?.let {
-//                adapter.setList(it.map { time ->
-//                    val deltaTime = viewModel.deltaTimeList[position]
-//                    time.plusMinutes(deltaTime.toLong())
-//                })
+        viewModel.deltaTimePosition.observe(viewLifecycleOwner) { position ->
+            if (sharedPref != null) {
+                with(sharedPref.edit()) {
+                    putInt(getString(R.string.alarmOffsetKey), position)
+                    apply()
+                }
             }
-            Log.i("observe", "viewModel.alarmOffsetTimeが変更された")
-        })
+        }
 
-        viewModel.setAlarmPosition.observe(viewLifecycleOwner, Observer { position ->
-            Log.i("observe","setAlarmPositionが変更された")
-            if (position != -1){
+        viewModel.setAlarmPosition.observe(viewLifecycleOwner) { position ->
+            if (position != -1) {
                 val alarmTime = viewModel.calcAlarm(position)
-                val hour = alarmTime.hour.toInt()
-                val minutes = alarmTime.minute.toInt()
+                val hour = alarmTime.hour
+                val minutes = alarmTime.minute
                 val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
                     putExtra(AlarmClock.EXTRA_HOUR, hour)
                     putExtra(AlarmClock.EXTRA_MINUTES, minutes)
                     putExtra(AlarmClock.EXTRA_SKIP_UI, true)
                 }
-                if (intent.resolveActivity(requireActivity().packageManager) != null){
+                if (intent.resolveActivity(requireActivity().packageManager) != null) {
                     startActivity(intent)
                 }
-                Toast.makeText(activity, "${resources.getString(R.string.set_alarm_complete_message)} ${alarmTime.toString()}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    activity,
+                    "${resources.getString(R.string.set_alarm_complete_message)} $alarmTime",
+                    Toast.LENGTH_SHORT
+                ).show()
                 viewModel.onSetAlarmPositionComplete()
             }
-        })
+        }
 
 
 
@@ -117,11 +96,6 @@ class ChoiceFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         viewModel.deltaTimePosition.value = position
-        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
-        with(sharedPref.edit()){
-            putInt(getString(R.string.alarmOffsetKey), position)
-            apply()
-        }
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
--- app/src/main/java/io/github/npeeech/saft/SharedViewModel.kt
+++ app/src/main/java/io/github/npeeech/saft/SharedViewModel.kt
@@ -1,11 +1,8 @@
 package io.github.npeeech.saft
 
-import android.graphics.Color
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ClickableSpan
-import android.text.style.ForegroundColorSpan
-import android.text.style.UnderlineSpan
 import android.util.Log
 import android.view.View
 import androidx.lifecycle.LiveData
@@ -17,20 +14,18 @@ class SharedViewModel(receiveText: String) : ViewModel() {
     val plainText = MutableLiveData<String>()
     val richText = MutableLiveData<SpannableString>()
 
+    private var alarmList = MutableLiveData<List<LocalTime>>()
+    private var deltaTimeList: List<Int> = listOf()
+
     private val _eventParse = MutableLiveData<Boolean>()
     val eventParse: LiveData<Boolean>
         get() = _eventParse
 
     val deltaTimePosition = MutableLiveData<Int>()
 
-    var alarmList = MutableLiveData<List<LocalTime>>()
-
-    var deltaTimeList: List<Int> = listOf()
-
-    private val _setAlarmPosition = MutableLiveData<Int>(-1)
+    private val _setAlarmPosition = MutableLiveData(-1)
     val setAlarmPosition: LiveData<Int>
         get() = _setAlarmPosition
-
 
     init {
         plainText.value = receiveText
@@ -45,43 +40,29 @@ class SharedViewModel(receiveText: String) : ViewModel() {
     }
 
     fun parse() {
-        Log.i("observe","クリックされた")
         val regex = Regex("""[0-2]?[0-9][:][0-6][0-9]""")
-
-        alarmList.value = regex.findAll(plainText.value ?: "").map {
-            LocalTime.parse(it.value)
-        }.toList()
+        val tmpAlarmList = mutableListOf<LocalTime>()
 
         richText.value = SpannableString(plainText.value)
         regex.findAll(plainText.value ?: "").withIndex().forEach {
-//            richText.value?.setSpan(
-//                ForegroundColorSpan(Color.BLUE),
-//                it.value.range.start,
-//                it.value.range.endInclusive+1,
-//                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-//            )
-//            richText.value?.setSpan(
-//                UnderlineSpan(),
-//                it.value.range.start,
-//                it.value.range.endInclusive+1,
-//                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-//            )
             richText.value?.setSpan(
-                object: ClickableSpan() {
+                object : ClickableSpan() {
                     override fun onClick(widget: View) {
-                        Log.i("observe","クリックされた")
+                        Log.i("observe", "クリックされた")
 
                         _setAlarmPosition.value = it.index
                     }
                 },
                 it.value.range.start,
-                it.value.range.endInclusive+1,
+                it.value.range.endInclusive + 1,
                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
             )
+            tmpAlarmList.add(LocalTime.parse(it.value.value))
         }
+        alarmList.value = tmpAlarmList
     }
 
-    fun onSetAlarmPositionComplete(){
+    fun onSetAlarmPositionComplete() {
         _setAlarmPosition.value = -1
     }
 
@@ -92,5 +73,9 @@ class SharedViewModel(receiveText: String) : ViewModel() {
     fun calcAlarm(position: Int): LocalTime {
         val alarmTime = alarmList.value?.get(position)!!
         return calcAlarm(alarmTime)
+    }
+
+    fun setDeltaTimeList(newDeltaTimeList: List<Int>) {
+        deltaTimeList = newDeltaTimeList
     }
 }
